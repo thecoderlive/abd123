0773f5e779d45d389a6b0b2aca9206f53bd37ba2
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-09-18 18:49:03 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "e0bb427e-8c87-4c40-912e-567ff157ba39",
+    "home_feed": [
+        {
+            "id": "2a555751-31f6-41f5-83b6-14eb4c8ad497",
+            "artist_name": "so",
+            "home_carousel": [
+                {
+                    "id": "f7445926-a628-43a0-bffb-bdd8e39aaf7c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/72.png"
+                },
+                {
+                    "id": "bb625b27-f5ad-4002-a091-0250236b3ee0",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/4.png"
+                },
+                {
+                    "id": "a8d46926-454b-40c7-907f-9e15b794880e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/38.png"
+                },
+                {
+                    "id": "94ed2b19-4fac-4f4d-8971-0721bf270d36",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/194.png"
+                },
+                {
+                    "id": "3e03ff7f-907b-4530-904a-df708de75c9b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/97.png"
+                }
+            ],
+            "art_caption": "good",
+            "art_description": "Director project deal beautiful star some tend. Land building fight institution."
+        },
+        {
+            "id": "7a034f83-b2ee-464e-96bf-5102c63f2d23",
+            "artist_name": "talk",
+            "home_carousel": [
+                {
+                    "id": "b3cd9b94-8cef-4292-8b5b-b08d17d54d09",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/10.png"
+                },
+                {
+                    "id": "837b0cfc-2921-422d-a89d-85f2e839da85",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/142.png"
+                },
+                {
+                    "id": "695fdd45-3e53-4a6b-94d0-9dddf74291bc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/254.png"
+                },
+                {
+                    "id": "6f313219-02be-4ee8-896f-2e32a6a1ce0f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/87.png"
+                },
+                {
+                    "id": "20bf722b-d2be-4d06-aaee-52714de975a3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/280.png"
+                }
+            ],
+            "art_caption": "protect",
+            "art_description": "Interesting build dinner minute after toward remember. Return use help."
+        },
+        {
+            "id": "13730d13-1881-47e9-9200-cf4bcc4ce397",
+            "artist_name": "young",
+            "home_carousel": [
+                {
+                    "id": "3d823ff5-fb99-4ab5-8fbe-f06ac2ae5bdc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/341.png"
+                },
+                {
+                    "id": "dd10abcb-4f3c-4568-9304-72050e9dc37d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/365.png"
+                },
+                {
+                    "id": "b660885d-a448-4afb-9de8-5d7322937523",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/360.png"
+                },
+                {
+                    "id": "e6aa0f04-ed92-4a09-8411-180d25016619",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/279.png"
+                },
+                {
+                    "id": "60caa722-e0f3-41e8-919f-eb50f7de3421",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/146.png"
+                }
+            ],
+            "art_caption": "challenge",
+            "art_description": "Ball would represent. Important same government case box focus worker."
+        },
+        {
+            "id": "afe88293-3c7d-44e2-9daa-ae10810830a0",
+            "artist_name": "soldier",
+            "home_carousel": [
+                {
+                    "id": "e57ac23d-8919-4367-86ea-8ce70b609df1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/310.png"
+                },
+                {
+                    "id": "4c0d76c6-eb59-454f-acf1-8a1ac0d4167e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/85.png"
+                },
+                {
+                    "id": "82d5e81b-8ca0-4275-bc2f-41f159f706ef",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/258.png"
+                },
+                {
+                    "id": "0ee45de5-2c8a-48c3-8f75-63b8434a845d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/163.png"
+                },
+                {
+                    "id": "8a17036a-3824-43c3-bf54-fd59540ec568",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/129.png"
+                }
+            ],
+            "art_caption": "first",
+            "art_description": "Be reveal police then order shoulder field. Because write treatment stop. Prepare computer her."
+        },
+        {
+            "id": "bc9c762c-1083-4b77-910c-4dde9fad7293",
+            "artist_name": "coach",
+            "home_carousel": [
+                {
+                    "id": "2c68d166-6dcf-4130-bf32-dcdc1a9a2b70",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/361.png"
+                },
+                {
+                    "id": "721fd33a-b0fd-4a4b-ba4b-c8737ee7a2bb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/223.png"
+                },
+                {
+                    "id": "0a01720f-00d2-401d-9f2d-54669ab9fd00",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/175.png"
+                },
+                {
+                    "id": "25c55bfd-daf5-4302-9d0f-0ab8ec691057",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/57.png"
+                },
+                {
+                    "id": "6ff0d021-3b31-4ea1-95f4-fe3ca95ba956",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/58.png"
+                }
+            ],
+            "art_caption": "range",
+            "art_description": "Including eight face budget house manage. Environment soldier election TV task herself."
+        }
+    ]
+}
